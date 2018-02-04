9dd2298c47a74087541210138c77ab8629418d46
pero5ar <pero.5ar@gmail.com>
2018-02-04 22:25:19 +0100

    add repository factories

---
+++ TravelBuddy.BaseLib/Factories/UnitOfWorkFactory.cs
@@ -0,0 +1,19 @@
+﻿using TravelBuddy.DAL;
+
+namespace TravelBuddy.BaseLib.Factories
+{
+    public class UnitOfWorkFactory
+    {
+        private static UnitOfWorkFactory _instance;
+
+        private UnitOfWorkFactory() { }
+
+        public static UnitOfWorkFactory GetInstance()
+        {
+            _instance = _instance ?? new UnitOfWorkFactory();
+            return _instance;
+        }
+
+        public IUnitOfWork CreateUnitOfWork() => new UnitOfWork();
+    }
+}
---
+++ TravelBuddy.WebApp/Models/TravelsViewModels/TravelViewModels.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TravelBuddy.WebApp.Models.TravelsViewModels
+{
+    public class TravelViewModels
+    {
+        public string Name { get; set; }
+    }
+}
